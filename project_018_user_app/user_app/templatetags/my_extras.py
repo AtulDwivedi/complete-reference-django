df886e3055330b8d91ab457d7fe4c22ac3326f2f
Atul Dwivedi <atul16dwivedi@gmail.com>
2019-12-09 14:23:17 +0530

    filters

---
+++ project_018_user_app/user_app/templatetags/my_extras.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+
+def cut(value, arg):
+    return value.replace(arg, '')
+
+
+register.filter('cut', cut)
--- project_018_user_app/user_app/views.py
+++ project_018_user_app/user_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'user_app/index.html', {})
+    dict = {'django': 'Django: The web framework for perfectionists with deadlines.'}
+    return render(request, 'user_app/index.html', dict)
 
 
 def user_registration(request):

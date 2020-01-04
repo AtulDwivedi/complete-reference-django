f566c10a980b6491dd3d7b40ae9c9732ec0fb4f5
Atul Dwivedi <atul16dwivedi@gmail.com>
2020-01-04 21:47:20 +0530

    Item Detail page

--- sample_projects/ecommerce/items/urls.py
+++ sample_projects/ecommerce/items/urls.py
@@ -5,4 +5,5 @@ app_name = 'items'
 
 urlpatterns = [
     url(r"^$", views.ListItems.as_view(), name="all"),
+    url(r"^(?P<pk>\d+)/", views.ItemDetail.as_view(), name="detail"),
 ]
--- sample_projects/ecommerce/items/views.py
+++ sample_projects/ecommerce/items/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from items.models import Item
 
+
 class ListItems(ListView):
+    model = Item
+
+
+class ItemDetail(DetailView):
     model = Item
